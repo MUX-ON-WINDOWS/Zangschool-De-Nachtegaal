e33f58fd80d8c613e5d464969caa609146bfc267
Max <maxnouts@gmail.com>
2023-02-09 22:03:24 +0100

    vue install

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import HelloWorld from './components/mainIndex.vue'
+import NavBar from './components/navBar.vue'
+</script>
+
+<template>
+  <main>
+    <NavBar/>
+  </main>
+  <main>
+    <HelloWorld/>
+  </main>
+</template>
---
+++ src/components/navBar.vue
@@ -0,0 +1,25 @@
+<script setup>
+import Home from '../App.vue'
+import Docenten from '../Docenten.vue'
+
+const routes = {
+    '/': Home,
+    '/docenten': Docenten
+}
+
+</script>
+
+<template>
+    <div>
+        <div class="title">
+            <p>Zangschool De Nachtegaal</p>
+        </div>
+        <div class="navbar">
+            <li><a href="#/">Home</a></li>
+            <li><a href="#/docenten">Docenten</a></li>
+            <li><a href="#">Zanglessen</a></li>
+            <li><a href="#">Prijzen</a></li>
+            <li><a href="#">Contact</a></li>
+        </div>
+    </div>
+</template>
